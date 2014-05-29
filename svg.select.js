616ce71398ccf6b321160b6327158c4ce569c31d
Ulrich-Matthias Schäfer <ulima.ums@googlemail.com>
2014-05-29 11:20:11 +0200

    Added circles at the edges

--- svg.select.js
+++ svg.select.js
@@ -1,28 +1,57 @@
 // svg.draw.js 0.0.5 - Copyright (c) 2014 Wout Fierens - Licensed under the MIT license
 // extended by Urich-Matthias Schäfer (https://github.com/Fuzzyma)
 
+// TODO: Maybe use nested SVG instead of group to avoid translation when moving
+
 ;(function() {
 
     SVG.extend(SVG.Element, {
         // Draw element with mouse
-        select: function(value) {
+        select: function(value, options) {
 
-            var update
+            var update, defaults
               , element = this
-              , value = value === null ? true : value
               , parent = this.parent._parent(SVG.Nested) || this._parent(SVG.Doc)
               , bbox = this.bbox();
 
-            // Deselect
+            defaults = {
+                points:true,
+                classRect:'svg_select_boundingRect',
+                classCircles:'svg_select_circles'
+            }
+
+            if(typeof value === 'object'){
+                options = value;
+                value = true;
+            }
+
+            value = value === null ? true : value;
+
+            for(var i in options){
+                defaults[i] = options[i];
+            }
+
+            // deselect
             if(!value){
-                this.boundingRect.attributeObserver.disconnect();
-                this.boundingRect && this.boundingRect.remove();
-                delete this.boundingRect;
+                if(this.boundingGroup){
+                    this.boundingGroup.attributeObserver.disconnect();
+                    this.boundingGroup.group.remove();
+                    delete this.boundingGroup;
+                }
+                this.isSelected = false;
                 return this;
             }
 
+            if(this.isSelected)return this;
+
             // select
-            this.boundingRect = this.parent.rect(bbox.width, bbox.height).move(bbox.x, bbox.y).attr({
+            this.boundingGroup = {
+                group:this.parent.group().move(bbox.x, bbox.y),
+                rect:null,
+                circles:[]
+            };
+
+            this.boundingGroup.rect = this.boundingGroup.group.rect(bbox.width, bbox.height).attr({
                 'stroke-width':1,
                 'fill':'gray',
                 'stroke-dasharray':'10 10',
@@ -32,18 +61,34 @@
                 'pointer-events':'none'
             });
 
+            if(defaults.points){
+                this.boundingGroup.circles.push(this.boundingGroup.group.circle(5).center(0,0));
+                this.boundingGroup.circles.push(this.boundingGroup.group.circle(5).center(bbox.width,0));
+                this.boundingGroup.circles.push(this.boundingGroup.group.circle(5).center(bbox.width,bbox.height));
+                this.boundingGroup.circles.push(this.boundingGroup.group.circle(5).center(0,bbox.height));
+            }
+
+            this.isSelected = true;
+
             // Observe the attributes of the shape. When changing, adjust the boundingRect
-            this.boundingRect.attributeObserver = new MutationObserver(function(){
+            this.boundingGroup.attributeObserver = new MutationObserver(function(){
                 bbox = element.bbox();
-                element.boundingRect && element.boundingRect.attr({
-                    width:bbox.width,
-                    height:bbox.height,
-                    x:bbox.x,
-                    y:bbox.y
-                });
+                if(element.boundingGroup){
+                    element.boundingGroup.group.move(bbox.x, bbox.y);
+                    element.boundingGroup.rect.attr({
+                        width:bbox.width,
+                        height:bbox.height
+                    });
+
+                    if(defaults.points){
+                        element.boundingGroup.circles[1].center(bbox.width,0);
+                        element.boundingGroup.circles[2].center(bbox.width,bbox.height);
+                        element.boundingGroup.circles[3].center(0,bbox.height);
+                    }
+                }
             });
 
-            this.boundingRect.attributeObserver.observe(this.node, {attributes:true});
+            this.boundingGroup.attributeObserver.observe(this.node, {attributes:true});
 
             return this;
         }
