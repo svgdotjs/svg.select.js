515cd4e7bc610c100794b423cafa5eb369f60607
Ulrich-Matthias Schäfer <ulima.ums@googlemail.com>
2014-06-01 19:48:30 +0200

    Added 4 more points (on edges) Added css-classes Removed unused variables Added preventDefault when clicking on points to prevent drag

--- README.md
+++ README.md
@@ -14,3 +14,25 @@ Select
 Unselect
 
     rect.select(false);
+
+
+You can style the selection with the classes
+
+- `svg_select_boundingRect`
+- `svg_select_points`
+- `svg_select_points_lt` - *left top*
+- `svg_select_points_rt` - *right top*
+- `svg_select_points_rb` - *right bottom*
+- `svg_select_points_lb` - *left bottom*
+- `svg_select_points_t`  - *top*
+- `svg_select_points_r`  - *right*
+- `svg_select_points_b`  - *bottom*
+- `svg_select_points_l`  - *left*
+
+
+# Options
+
+- points: Points should be drawn (default `true`)
+- classRect: Classname of the rect from the bounding Box (default `svg_select_boundingRect`)
+- classPoints: Classname/Prefix of the Points (default `svg_select_points`)
+- radius: Radius of the points (default `7`)
--- svg.select.js
+++ svg.select.js
@@ -1,33 +1,34 @@
-// svg.draw.js 0.0.5 - Copyright (c) 2014 Wout Fierens - Licensed under the MIT license
-// extended by Urich-Matthias Schäfer (https://github.com/Fuzzyma)
+// svg.draw.js 0.1.0 - Copyright (c) 2014 Urich-Matthias Schäfer - Licensed under the MIT license
 
 // TODO: Maybe use nested SVG instead of group to avoid translation when moving
 
 ;(function() {
 
     SVG.extend(SVG.Element, {
-        // Draw element with mouse
+        // Select element with mouse
         select: function(value, options) {
 
-            var update, defaults
+            var defaults
               , element = this
-              , parent = this.parent._parent(SVG.Nested) || this._parent(SVG.Doc)
               , bbox = this.bbox();
 
             defaults = {
                 points:true,
                 classRect:'svg_select_boundingRect',
-                classCircles:'svg_select_circles'
-            }
+                classPoints:'svg_select_points',
+                radius:7
+                //TODO: deepSelect - doubleClick selects every point from polygons/lines and not only the box
+            };
 
             if(typeof value === 'object'){
                 options = value;
                 value = true;
             }
 
-            value = value === null ? true : value;
+            value = value === undefined ? true : value;
 
             for(var i in options){
+                if(!defaults[i])throw('Property '+i+'doesn\'t exists');
                 defaults[i] = options[i];
             }
 
@@ -52,20 +53,36 @@
             };
 
             this.boundingGroup.rect = this.boundingGroup.group.rect(bbox.width, bbox.height).attr({
-                'stroke-width':1,
-                'fill':'gray',
-                'stroke-dasharray':'10 10',
-                'stroke':'black',
-                'stroke-opacity':0.8,
-                'fill-opacity':0.1,
-                'pointer-events':'none'
+                'class':defaults.classRect
             });
 
             if(defaults.points){
-                this.boundingGroup.circles.push(this.boundingGroup.group.circle(5).center(0,0));
-                this.boundingGroup.circles.push(this.boundingGroup.group.circle(5).center(bbox.width,0));
-                this.boundingGroup.circles.push(this.boundingGroup.group.circle(5).center(bbox.width,bbox.height));
-                this.boundingGroup.circles.push(this.boundingGroup.group.circle(5).center(0,bbox.height));
+                this.boundingGroup.circles.push(this.boundingGroup.group.circle(defaults.radius).center(0,0).attr('class', defaults.classPoints+'_lt')
+                    .mousedown(function(ev){ ev.preventDefault && ev.preventDefault(); element.node.dispatchEvent(new CustomEvent('lt', {detail:{x:ev.pageX, y:ev.pageY}})); }));
+
+                this.boundingGroup.circles.push(this.boundingGroup.group.circle(defaults.radius).center(bbox.width,0).attr('class', defaults.classPoints+'_rt')
+                    .mousedown(function(ev){ ev.preventDefault && ev.preventDefault(); element.node.dispatchEvent(new CustomEvent('rt', {detail:{x:ev.pageX, y:ev.pageY}})); }));
+
+                this.boundingGroup.circles.push(this.boundingGroup.group.circle(defaults.radius).center(bbox.width,bbox.height).attr('class', defaults.classPoints+'_rb')
+                    .mousedown(function(ev){ ev.preventDefault && ev.preventDefault(); element.node.dispatchEvent(new CustomEvent('rb', {detail:{x:ev.pageX, y:ev.pageY}})); }));
+
+                this.boundingGroup.circles.push(this.boundingGroup.group.circle(defaults.radius).center(0,bbox.height).attr('class', defaults.classPoints+'_lb')
+                    .mousedown(function(ev){ ev.preventDefault && ev.preventDefault(); element.node.dispatchEvent(new CustomEvent('lb', {detail:{x:ev.pageX, y:ev.pageY}})); }));
+
+
+                this.boundingGroup.circles.push(this.boundingGroup.group.circle(defaults.radius).center(bbox.width/2,0).attr('class', defaults.classPoints+'_t')
+                    .mousedown(function(ev){ ev.preventDefault && ev.preventDefault(); element.node.dispatchEvent(new CustomEvent('t', {detail:{x:ev.pageX, y:ev.pageY}})); }));
+
+                this.boundingGroup.circles.push(this.boundingGroup.group.circle(defaults.radius).center(bbox.width,bbox.height/2).attr('class', defaults.classPoints+'_r')
+                    .mousedown(function(ev){ ev.preventDefault && ev.preventDefault(); element.node.dispatchEvent(new CustomEvent('r', {detail:{x:ev.pageX, y:ev.pageY}})); }));
+
+                this.boundingGroup.circles.push(this.boundingGroup.group.circle(defaults.radius).center(bbox.width/2,bbox.height).attr('class', defaults.classPoints+'_b')
+                    .mousedown(function(ev){ ev.preventDefault && ev.preventDefault(); element.node.dispatchEvent(new CustomEvent('b', {detail:{x:ev.pageX, y:ev.pageY}})); }));
+
+                this.boundingGroup.circles.push(this.boundingGroup.group.circle(defaults.radius).center(0,bbox.height/2).attr('class', defaults.classPoints+'_l')
+                    .mousedown(function(ev){ ev.preventDefault && ev.preventDefault(); element.node.dispatchEvent(new CustomEvent('l', {detail:{x:ev.pageX, y:ev.pageY}})); }));
+
+                this.boundingGroup.circles.forEach(function(c){ c.node.className += ' ' + defaults.classPoints; });
             }
 
             this.isSelected = true;
@@ -84,6 +101,11 @@
                         element.boundingGroup.circles[1].center(bbox.width,0);
                         element.boundingGroup.circles[2].center(bbox.width,bbox.height);
                         element.boundingGroup.circles[3].center(0,bbox.height);
+
+                        element.boundingGroup.circles[4].center(bbox.width/2,0);
+                        element.boundingGroup.circles[5].center(bbox.width,bbox.height/2);
+                        element.boundingGroup.circles[6].center(bbox.width/2,bbox.height);
+                        element.boundingGroup.circles[7].center(0,bbox.height/2);
                     }
                 }
             });
