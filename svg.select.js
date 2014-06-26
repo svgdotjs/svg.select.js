fbf37fa6f924c38f7daa58fde4f94e0c55d07bbd
Ulrich-Matthias Schäfer <ulima.ums@googlemail.com>
2014-06-26 20:44:35 +0200

    Cleaned and commented. Added old mutation events if MutationObserver is not supported

--- svg.select.js
+++ svg.select.js
@@ -1,171 +1,249 @@
-// svg.draw.js 0.1.0 - Copyright (c) 2014 Urich-Matthias Schäfer - Licensed under the MIT license
+// svg.draw.js 0.1.0 - Copyright (c) 2014 Ulrich-Matthias Schäfer - Licensed under the MIT license
 
-// TODO: Maybe use nested SVG instead of group to avoid translation when moving
-
-;(function() {
+;(function () {
 
     SVG.extend(SVG.Element, {
         // Select element with mouse
-        select: function(value, options) {
+        select: function (value, options) {
 
-            var defaults
-              , element = this
-              , bbox = this.bbox()
-              , parent = this.parent._parent(SVG.Nested) || this._parent(SVG.Doc);
+            var defaults, observer
+                , element = this
+                , bbox = this.bbox()
+                , parent = this.parent._parent(SVG.Nested) || this._parent(SVG.Doc);
 
+            // The defaults for our option-object
             defaults = {
-                points:true,
-                classRect:'svg_select_boundingRect',
-                classPoints:'svg_select_points',
-                radius:7,
-                rotationPoint:true,
-                deepSelect:false
+                points: true,                            // If true, points at the edges are drawn. Needed for resize!
+                classRect: 'svg_select_boundingRect',    // Css-class added to the rect
+                classPoints: 'svg_select_points',        // Css-class added to the points
+                radius: 7,                               // radius of the points
+                rotationPoint: true,                     // If true, rotation point is drawn. Needed for rotation!
+                deepSelect: false                        // If true, moving of single points is possible (only line, polyline, polyon)
             };
 
-            if(typeof value === 'object'){
+            // Check the parameters and reassign if needed
+            if (typeof value === 'object') {
                 options = value;
                 value = true;
             }
 
+            // When no parameter is given, our value is true
             value = value === undefined ? true : value;
 
-            for(var i in options){
-                if(defaults[i] === undefined)throw('Property '+i+' doesn\'t exists');
+            // Merging the defaults and the options-object together
+            for (var i in options) {
+                if (!defaults.hasOwnProperty(i)) throw('Property ' + i + ' doesn\'t exists');
                 defaults[i] = options[i];
             }
 
-            if(defaults.deepSelect && ['line', 'polyline', 'polygon'].indexOf(element.type) !== -1){
-                var drawCircles, updateCircles, array;
+
+            // START DEEPSELECT
+
+            // When deepSelect is enabled and the element is a line/polyline/polyline, draw only points for moving
+            if (defaults.deepSelect && ['line', 'polyline', 'polygon'].indexOf(element.type) !== -1) {
+                var drawCircles, updateCircles, array, observeDeepSelectedPoints;
                 element.deepSelect = element.deepSelect || {};
 
-                if(!value){
-                    element.deepSelect.set.each(function(){ this.remove(); });
+                if (!value) {
+                    // Remove all points, clear the set, stop watching the element
+                    element.deepSelect.set.each(function () {
+                        this.remove();
+                    });
                     element.deepSelect.set.clear();
                     element.isSelected = false;
-                    this.deepSelect.attributeObserver.disconnect();
+
+                    // create an MutationObserver if possible, fallback to mutation-events if not
+                    if (MutationObserver) {
+                        this.deepSelect.attributeObserver.disconnect();
+                    } else {
+                        element.off(observeDeepSelectedPoints);
+                    }
+
                     return this;
                 }
 
-                if(element.isSelected)return this;
+                // we don't need to select twice
+                if (element.isSelected)return this;
 
+                // Create our set of elements
                 element.deepSelect.set = parent.set();
 
-                drawCircles = function(array){
-                    for(var i = 0; i<array.length; ++i){
+                // The function to draw the circles
+                drawCircles = function (array) {
+
+                    // go through the array of points
+                    for (var i = 0, len = array.length; i < len; ++i) {
+
+                        // add every point to the set
                         element.deepSelect.set.add(
-                            parent.circle(defaults.radius).center(array[i][0], array[i][1]).attr('class', defaults.classPoints + ' ' + defaults.classPoints+'_point')
+
+                            // a circle with our css-classes and a mousedown-event which fires our event for moving points
+                            parent.circle(defaults.radius).center(array[i][0], array[i][1]).addClass(defaults.classPoints).addClass(defaults.classPoints + '_point')
                                 .mousedown(
-                                    (function(k){
-                                        return function(ev){
+                                    (function (k) {
+                                        return function (ev) {
                                             ev.preventDefault && ev.preventDefault();
-                                            element.node.dispatchEvent(new CustomEvent('point', {detail:{x:ev.pageX, y:ev.pageY, i:k}}));
+                                            element.node.dispatchEvent(new CustomEvent('point', {detail: {x: ev.pageX, y: ev.pageY, i: k}}));
                                         }
                                     })(i)
                                 )
                         );
                     }
+
                 };
 
-                updateCircles = function(array){
-                    element.deepSelect.set.each(function(i){
-                        if(this.cx() == array[i][0] && this.cy() == array[i][1])return;
+                // every time a circle is moved, we have to update the positions of our circle
+                updateCircles = function (array) {
+                    element.deepSelect.set.each(function (i) {
+                        if (this.cx() == array[i][0] && this.cy() == array[i][1])return;
                         this.center(array[i][0], array[i][1]);
                     });
-                }
-                array = element.type == 'line' ? [[element.attr('x1'), element.attr('y1')],[element.attr('x2'), element.attr('y2')]] : element.array.value;
+                };
 
+                // create the point-array which contains the 2 points of a line or simply the points-array of polyline/polygon
+                array = element.type == 'line' ? [
+                    [element.attr('x1'), element.attr('y1')],
+                    [element.attr('x2'), element.attr('y2')]
+                ] : element.array.value;
+
+                // draw the circles and mark the element as selected
                 drawCircles(array);
                 element.isSelected = true;
 
-                this.deepSelect.attributeObserver = new MutationObserver(function(){
-                    var arr = element.type == 'line' ? [[element.attr('x1'), element.attr('y1')],[element.attr('x2'), element.attr('y2')]] : element.array.value;
-                    updateCircles(arr);
-                });
 
-                this.deepSelect.attributeObserver.observe(element.node, {attributes:true});
+                // Observer which is triggered, when point is moved
+                observeDeepSelectedPoints = function() {
+                    var arr = element.type == 'line' ? [
+                        [element.attr('x1'), element.attr('y1')],
+                        [element.attr('x2'), element.attr('y2')]
+                    ] : element.array.value;
+                    updateCircles(arr);
+                };
+
+                // create an MutationObserver if possible, fallback to mutation-events if not
+                if (MutationObserver) {
+
+                    // observe the attributes of the shape. When changing, adjust the boundingRect
+                    this.deepSelect.attributeObserver = new MutationObserver(observeDeepSelectedPoints);
+                    this.deepSelect.attributeObserver.observe(this.node, {attributes: true});
+
+                } else {
+                    element.on('DOMAttrModified', observeDeepSelectedPoints);
+                }
+
                 return this;
             }
 
-            // deselect
-            if(!value){
-                if(this.boundingGroup){
-                    this.boundingGroup.attributeObserver.disconnect();
+            // END DEEPSELECT
+
+
+            // Deselect the element
+            if (!value) {
+
+                // stop watching the element, remove the selection
+                if (this.boundingGroup) {
+                    if (MutationObserver) {
+                        this.boundingGroup.attributeObserver.disconnect();
+                    } else {
+                        element.off(observer);
+                    }
                     this.boundingGroup.group.remove();
                     delete this.boundingGroup;
                 }
+
+                // mark element as not selected
                 this.isSelected = false;
                 return this;
             }
 
-            if(this.isSelected)return this;
+            // don't select twice
+            if (this.isSelected)return this;
 
-            // select
+            // our object to store the elements we use
             this.boundingGroup = {
-                group:this.parent.group().transform(element.transform()).move(bbox.x, bbox.y),
-                rect:null,
-                circles:[]
+                group: this.parent.nested().transform(element.transform()).move(bbox.x, bbox.y),
+                rect: null,
+                circles: []
             };
 
-            this.boundingGroup.rect = this.boundingGroup.group.rect(bbox.width, bbox.height).attr({
-                'class':defaults.classRect
-            });
-
-            function getMoseDownFunc(eventName){
-                return function(ev){
+            // helperFunction to create a mouse-down function which triggers the event specified in `eventName`
+            function getMoseDownFunc(eventName) {
+                return function (ev) {
                     (ev.preventDefault && ev.preventDefault()) || (ev.returnValue = false);
-                    element.node.dispatchEvent(new CustomEvent(eventName, {detail:{x:ev.pageX, y:ev.pageY}}));
+                    element.node.dispatchEvent(new CustomEvent(eventName, {detail: {x: ev.pageX, y: ev.pageY}}));
                 };
             }
 
-            if(defaults.points){
-                this.boundingGroup.circles.push(this.boundingGroup.group.circle(defaults.radius).center(0,0).attr('class', defaults.classPoints+'_lt').mousedown(getMoseDownFunc('lt')));
-                this.boundingGroup.circles.push(this.boundingGroup.group.circle(defaults.radius).center(bbox.width,0).attr('class', defaults.classPoints+'_rt').mousedown(getMoseDownFunc('rt')));
-                this.boundingGroup.circles.push(this.boundingGroup.group.circle(defaults.radius).center(bbox.width,bbox.height).attr('class', defaults.classPoints+'_rb').mousedown(getMoseDownFunc('rb')));
-                this.boundingGroup.circles.push(this.boundingGroup.group.circle(defaults.radius).center(0,bbox.height).attr('class', defaults.classPoints+'_lb').mousedown(getMoseDownFunc('lb')));
+            // create the selection-rectangle and add the css-class
+            this.boundingGroup.rect = this.boundingGroup.group.rect(bbox.width, bbox.height).addClass(defaults.classRect);
 
-                this.boundingGroup.circles.push(this.boundingGroup.group.circle(defaults.radius).center(bbox.width/2,0).attr('class', defaults.classPoints+'_t').mousedown(getMoseDownFunc('t')));
-                this.boundingGroup.circles.push(this.boundingGroup.group.circle(defaults.radius).center(bbox.width,bbox.height/2).attr('class', defaults.classPoints+'_r').mousedown(getMoseDownFunc('r')));
-                this.boundingGroup.circles.push(this.boundingGroup.group.circle(defaults.radius).center(bbox.width/2,bbox.height).attr('class', defaults.classPoints+'_b').mousedown(getMoseDownFunc('b')));
-                this.boundingGroup.circles.push(this.boundingGroup.group.circle(defaults.radius).center(0,bbox.height/2).attr('class', defaults.classPoints+'_l').mousedown(getMoseDownFunc('l')));
+            // Draw Points at the edges, if enabled
+            if (defaults.points) {
+                this.boundingGroup.circles.push(this.boundingGroup.group.circle(defaults.radius).center(0, 0).attr('class', defaults.classPoints + '_lt').mousedown(getMoseDownFunc('lt')));
+                this.boundingGroup.circles.push(this.boundingGroup.group.circle(defaults.radius).center(bbox.width, 0).attr('class', defaults.classPoints + '_rt').mousedown(getMoseDownFunc('rt')));
+                this.boundingGroup.circles.push(this.boundingGroup.group.circle(defaults.radius).center(bbox.width, bbox.height).attr('class', defaults.classPoints + '_rb').mousedown(getMoseDownFunc('rb')));
+                this.boundingGroup.circles.push(this.boundingGroup.group.circle(defaults.radius).center(0, bbox.height).attr('class', defaults.classPoints + '_lb').mousedown(getMoseDownFunc('lb')));
 
-                this.boundingGroup.circles.forEach(function(c){ c.node.className += ' ' + defaults.classPoints; });
+                this.boundingGroup.circles.push(this.boundingGroup.group.circle(defaults.radius).center(bbox.width / 2, 0).attr('class', defaults.classPoints + '_t').mousedown(getMoseDownFunc('t')));
+                this.boundingGroup.circles.push(this.boundingGroup.group.circle(defaults.radius).center(bbox.width, bbox.height / 2).attr('class', defaults.classPoints + '_r').mousedown(getMoseDownFunc('r')));
+                this.boundingGroup.circles.push(this.boundingGroup.group.circle(defaults.radius).center(bbox.width / 2, bbox.height).attr('class', defaults.classPoints + '_b').mousedown(getMoseDownFunc('b')));
+                this.boundingGroup.circles.push(this.boundingGroup.group.circle(defaults.radius).center(0, bbox.height / 2).attr('class', defaults.classPoints + '_l').mousedown(getMoseDownFunc('l')));
+
+                this.boundingGroup.circles.forEach(function (c) {
+                    c.addClass(defaults.classPoints);
+                });
             }
 
-            if(defaults.rotationPoint){
-                this.boundingGroup.circles.push(this.boundingGroup.group.circle(defaults.radius).center(bbox.width/2,20).attr('class', defaults.classPoints+'_rot')
-                    .mousedown(function(ev){ ev.preventDefault && ev.preventDefault(); element.node.dispatchEvent(new CustomEvent('rot', {detail:{x:ev.pageX, y:ev.pageY}})); }));
+            // draw rotationPint, if enabled
+            if (defaults.rotationPoint) {
+                this.boundingGroup.circles.push(this.boundingGroup.group.circle(defaults.radius).center(bbox.width / 2, 20).attr('class', defaults.classPoints + '_rot')
+                    .mousedown(function (ev) {
+                        ev.preventDefault && ev.preventDefault();
+                        element.node.dispatchEvent(new CustomEvent('rot', {detail: {x: ev.pageX, y: ev.pageY}}));
+                    }));
             }
 
+            // mark element as selected
             this.isSelected = true;
 
-            // Observe the attributes of the shape. When changing, adjust the boundingRect
-            this.boundingGroup.attributeObserver = new MutationObserver(function(){
+            // Observer which is triggered on resize and drag
+            observer = function() {
+
+                // Move all elements to the new position
                 bbox = element.bbox();
-                if(element.boundingGroup){
+                if (element.boundingGroup) {
                     element.boundingGroup.group.transform(element.transform()).move(bbox.x, bbox.y);
                     element.boundingGroup.rect.attr({
-                        width:bbox.width,
-                        height:bbox.height
+                        width: bbox.width,
+                        height: bbox.height
                     });
 
-                    if(defaults.points){
-                        element.boundingGroup.circles[1].center(bbox.width,0);
-                        element.boundingGroup.circles[2].center(bbox.width,bbox.height);
-                        element.boundingGroup.circles[3].center(0,bbox.height);
+                    if (defaults.points) {
+                        element.boundingGroup.circles[1].center(bbox.width, 0);
+                        element.boundingGroup.circles[2].center(bbox.width, bbox.height);
+                        element.boundingGroup.circles[3].center(0, bbox.height);
 
-                        element.boundingGroup.circles[4].center(bbox.width/2,0);
-                        element.boundingGroup.circles[5].center(bbox.width,bbox.height/2);
-                        element.boundingGroup.circles[6].center(bbox.width/2,bbox.height);
-                        element.boundingGroup.circles[7].center(0,bbox.height/2);
+                        element.boundingGroup.circles[4].center(bbox.width / 2, 0);
+                        element.boundingGroup.circles[5].center(bbox.width, bbox.height / 2);
+                        element.boundingGroup.circles[6].center(bbox.width / 2, bbox.height);
+                        element.boundingGroup.circles[7].center(0, bbox.height / 2);
                     }
 
-                    if(defaults.rotationPoint){
-                        element.boundingGroup.circles[8].center(bbox.width/2,20);
+                    if (defaults.rotationPoint) {
+                        element.boundingGroup.circles[8].center(bbox.width / 2, 20);
                     }
                 }
-            });
+            };
 
-            this.boundingGroup.attributeObserver.observe(this.node, {attributes:true});
+            // create an MutationObserver if possible, fallback to mutation-events if not
+            if (MutationObserver) {
+                // observe the attributes of the shape. When changing, adjust the boundingRect
+                this.boundingGroup.attributeObserver = new MutationObserver(observer);
+                this.boundingGroup.attributeObserver.observe(this.node, {attributes: true});
+
+            } else {
+                element.on('DOMAttrModified', observer);
+            }
 
             return this;
         }
