7057b69fc1adb09f3fdfec786812d1a126e3c523
Ulrich-Matthias Schäfer <ulima.ums@googlemail.com>
2016-04-28 14:47:55 +0200

    bower

--- dist/svg.select.js
+++ dist/svg.select.js
@@ -1,4 +1,4 @@
-/*! svg.select.js - v2.0.2 - 2016-03-12
+/*! svg.select.js - v2.0.3 - 2016-04-28
 * https://github.com/Fuzzyma/svg.select.js
 * Copyright (c) 2016 Ulrich-Matthias Schäfer; Licensed MIT */
 /*jshint -W083*/
