c9d695421dfdc54a19d9e1c6bfc23ecee11be572
Ulrich-Matthias Schäfer <ulima.ums@googlemail.com>
2018-06-07 19:23:22 +0200

    bump 3.0

--- dist/svg.select.js
+++ dist/svg.select.js
@@ -1,6 +1,6 @@
 /*!
 * svg.select.js - An extension of svg.js which allows to select elements with mouse
-* @version 2.1.2
+* @version 3.0.0
 * https://github.com/svgdotjs/svg.select.js
 *
 * @copyright Ulrich-Matthias Schäfer
