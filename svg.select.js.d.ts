af258804b7d259fa3c63f89a489dded3836dbf28
Ulrich-Matthias Schäfer <ulima.ums@googlemail.com>
2024-06-30 20:44:51 +0200

    fix types

--- svg.select.js.d.ts
+++ svg.select.js.d.ts
@@ -1,27 +1,40 @@
-import { SelectHandler } from './src/SelectHandler.js'
+import { SelectHandler, PointSelectHandler } from './src/SelectHandler.js'
 
-interface selectionPointArray {
-  [index: number]: 'lt' | 'rt' | 'rb' | 'lb' | 't' | 'r' | 'b' | 'l' | 'rot' | 'shear'
-}
-
-interface selectionOptions {
-  points: selectionPointArray
-  pointsExclude: selectionPointArray
-  classRect: string
-  classPoints: string
-  pointType: 'circle' | 'rect'
-  pointSize: number
-  rotationPoint: boolean
-  deepSelect: boolean
+interface SelectionOptions {
+  createHandle?: (el: Element) => Element
+  updateHandle?: (el: Element, point: number[]) => void
+  createRot?: (el: Element) => Element
+  updateRot?: (el: Element, rotPoint: number[], handlePoints: number[][]) => void
 }
 
 declare module '@svgdotjs/svg.js' {
   interface Element {
     select(): this
-
     select(enable: boolean): this
-    select(options: selectionOptions): this
+    select(options: SelectionOptions): this
     select(handler: SelectHandler): this
-    select(attr?: SelectHandler | selectionOptions | boolean): this
+    select(attr?: SelectHandler | SelectionOptions | boolean): this
+  }
+
+  interface Polygon {
+    pointSelect(): this
+    pointSelect(enable: boolean): this
+    pointSelect(options: SelectionOptions): this
+    pointSelect(handler: PointSelectHandler): this
+    pointSelect(attr?: PointSelectHandler | SelectionOptions | boolean): this
+  }
+  interface Polyline {
+    pointSelect(): this
+    pointSelect(enable: boolean): this
+    pointSelect(options: SelectionOptions): this
+    pointSelect(handler: PointSelectHandler): this
+    pointSelect(attr?: PointSelectHandler | SelectionOptions | boolean): this
+  }
+  interface Line {
+    pointSelect(): this
+    pointSelect(enable: boolean): this
+    pointSelect(options: SelectionOptions): this
+    pointSelect(handler: PointSelectHandler): this
+    pointSelect(attr?: PointSelectHandler | SelectionOptions | boolean): this
   }
 }
